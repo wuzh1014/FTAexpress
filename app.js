68d4794a43d531478cb787e2bf4daf07773ea7c1
willis <357096630@qq.com>
2018-03-26 00:29:29 +0800

    change origin

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let app = express();
 let http = require('http');
 let server = http.createServer(app);
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'http://localhost:7456');
+    res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Authorization');
